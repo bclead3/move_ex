b8a3ca7c5a9ac8afbfa4721096d55be159f1abf1
Ben Leadholm <bclead3@yahoo.com>
2013-08-07 06:39:11 -0600

    initial stash

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MoveEx::Application.config.secret_key_base = '536dbde00eda81cf9695f5b3e76670bd3b836e0079f475da34c2a35839331606fb1961de857755e9bc5e5c31844954f9b15ff88c8fac4d08496931ba10507edd'
